9020a2a73cdd14d2df12629e86d1e9e70158c873
Ethan Rublee <ethan.rublee@gmail.com>
2020-08-17 15:31:41 -0700

    Adding some system requirements. (#36)
    * Adding some system requirements.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,19 +9,29 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 export FARM_NG_ROOT=$( cd "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
 
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+
 # install system deps, punt for now.
-# sudo apt-get update
-# sudo apt-get install -y \
-#      python3-pip
-# sudo pip3 install virtualenv
+sudo apt-get update
+sudo apt-get install apt-utils -y
+sudo apt-get install -y \
+     build-essential \
+     cmake \
+     libboost-filesystem-dev \
+     libboost-regex-dev \
+     libboost-system-dev \
+     libprotobuf-dev \
+     librealsense2-dev \
+     librealsense2-utils \
+     protobuf-compiler \
+     python3-pip
+
+sudo pip3 install virtualenv
 
 if [ ! -f $FARM_NG_ROOT/env/bin/activate ]; then
-    virtualenv $FARM_NG_ROOT/env    
+    virtualenv $FARM_NG_ROOT/env
 fi
 
 . $FARM_NG_ROOT/env/bin/activate
 pip install -r $FARM_NG_ROOT/requirements.txt
-
-
-
-
