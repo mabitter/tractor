30095eac45d9417510f0ff5afd38c8c0112fa4f0
Ethan Rublee <ethan.rublee@gmail.com>
2020-09-09 08:22:49 -0700

    Add indexing support. (#72)
    * Add indexing support.
    - https://youtu.be/NXBSp4u8ZTE
    
    * Linter/whitespace.

--- python/farm_ng/steering.py
+++ python/farm_ng/steering.py
@@ -54,52 +54,147 @@ class SteeringSenderJoystick:
         loop = asyncio.get_event_loop()
         self.rate_hz = 50.0
         self.period = 1.0/self.rate_hz
+
+        # whether the tractor is executing a motion primitive (e.g. cruise control, indexing)
         self._executing_motion_primitive = False
+
+        # rate that the speed of the tractor changes when pressing the dpad up/down arrows
         self._delta_x_vel = 0.25/self.rate_hz
+        # rate of angular velocity that occurs when nudging the tractor left/right with dpad left/right arrows
         self._delta_angular_vel = np.pi/16
+
+        # maximum acceleration for manual steering and motion primitives
+        self._max_acc = 1.5/self.rate_hz  # m/s^2
+        # The target speed, used for smoothing out the commanded velocity to respect the max_acc limit
+        self._target_speed = 0.0
 
         self.joystick = MaybeJoystick('/dev/input/js0',  loop)
         self.joystick.set_button_callback(self.on_button)
         self._periodic = Periodic(self.period, loop, self.send)
         self._command = SteeringCommand()
 
+        # indexing state machine variables
+        # ammount of time spent moving during indexing, if None, will move forever
+        self._moving_duration = None
+        # amount of time spent stopping during indexing, if None, will stop forever
+        self._stopping_duration = None
+        # whether we are indexing or not
+        self._indexing = False
+        # Indexing state machine state, can be one of 'starting', 'moving', 'stopping'
+        self._indexing_state = 'starting'
+        # The last set indexing speed, to be applied while in 'moving' state
+        self._indexing_speed = 0.0
+        # When moving started
+        self._moving_start_time = None
+        # When stopping started
+        self._stopping_start_time = None
+
+    def start_motion_primitive(self):
+        self._executing_motion_primitive = True
+        self._command.brake = 0.0
+        self._command.deadman = 0.0
+
     def stop(self):
         self._command.velocity = 0.0
         self._command.angular_velocity = 0.0
         self._command.brake = 1.0
+        self._target_speed = 0.0
         self._command.deadman = 0.0
+        self._indexing = False
         self._executing_motion_primitive = False
 
     def on_button(self, button, value):
         if button == 'touch' and value:
             self.stop()
 
+        if button == 'cross' and value:
+            # Pressing cross causes the indexing behavor to toggle on/off.
+            # Pressing cross will recall the last indexing speed and moving/stopping durations.
+            self._indexing = not self._indexing
+            if not self._indexing:
+                self.stop()
+            else:
+                self.enter_indexing_state('starting')
+
+        if button == 'square' and value:
+            # pressing 'square' causes the indexing behavior to stop
+            # and reset indexing moving/stopping durations and speed settings
+            self.stop()
+            self._indexing_speed = 0.0
+            self._moving_duration = None
+            self._stopping_duration = None
+
+        if button == 'triangle' and self._indexing:
+            # Measures the time that the triangle button is pressed, and sets the moving duration to the measured time.
+            # The steering enteres the moving state, so the tractor may move (if there is an indexing_speed set)
+            if value:
+                self.enter_indexing_state('moving')
+                self._moving_duration = None
+            else:
+                self._moving_duration = time.time() - self._moving_start_time
+                print('index duration', self._moving_duration)
+
+        if button == 'circle' and self._indexing:
+            # Measures the time that the circle button is pressed, and sets the stopping duration to the measured time.
+            # This also causes the steering to enter the stopping state, so the tractor stops while the circle button is pressed.
+            if value:
+                self.enter_indexing_state('stopping')
+                self._stopping_duration = None
+            else:
+                self._stopping_duration = time.time() - self._stopping_start_time
+                print('pause duration: ', self._stopping_duration)
+
+    def enter_indexing_state(self, state_name):
+        self._indexing_state = state_name
+        if self._indexing_state == 'starting':
+            self.start_motion_primitive()
+        if self._indexing_state == 'moving':
+            self._target_speed = self._indexing_speed
+            self._moving_start_time = time.time()
+        if self._indexing_state == 'stopping':
+            self._stopping_start_time = time.time()
+            self._target_speed = 0.0
+
     def send(self, n_periods):
         if self.joystick.get_axis_state('hat0y', 0.0) != 0:
-            self._executing_motion_primitive = True
+            self.start_motion_primitive()
             # hat0y -1 is up dpad
             # hat0y +1 is down dpad
             inc_vel = -self.joystick.get_axis_state('hat0y', 0.0)*self._delta_x_vel
-            self._command.brake = 0.0
-            self._command.deadman = 0.0
             self._command.angular_velocity = 0.0
-            self._command.velocity = np.clip(
-                self._command.velocity + inc_vel,
+
+            self._target_speed = np.clip(
+                self._target_speed + inc_vel,
                 -2, 2,
             )
+            if self._indexing and self._indexing_state == 'moving':
+                self._indexing_speed = self._target_speed
 
         if self.joystick.get_axis_state('hat0x', 0.0) != 0:
-            self._executing_motion_primitive = True
+            self.start_motion_primitive()
             # hat0x -1 is left dpad
             # hat0x +1 is right dpad
-            inc_vel = -self.joystick.get_axis_state('hat0x', 0.0)*self._delta_angular_vel
-            self._command.brake = 0.0
-            self._command.deadman = 0.0
-            self._command.angular_velocity = inc_vel
+            angular_vel = -self.joystick.get_axis_state('hat0x', 0.0)*self._delta_angular_vel
+            self._command.angular_velocity = angular_vel
+
         elif self._executing_motion_primitive:
             # angular velocity resets if left or right dpad is not pressed,
             # so its like a nudge rather than cruise control.
             self._command.angular_velocity = 0.0
+
+        if self._executing_motion_primitive and self._indexing:
+            if self._indexing_state == 'starting':
+                self.enter_indexing_state('moving')
+
+            elif self._indexing_state == 'moving':
+                self._target_speed = self._indexing_speed
+                if self._moving_duration is not None and time.time() - self._moving_start_time > self._moving_duration:
+                    self.enter_indexing_state('stopping')
+
+            elif self._indexing_state == 'stopping':
+                self._target_speed = 0.0
+                if self._stopping_duration is not None and (time.time() - self._stopping_start_time) > self._stopping_duration:
+                    self.enter_indexing_state('moving')
 
         if not self._executing_motion_primitive:
             if not self.joystick.get_button_state('L2', False) or not self.joystick.is_connected() or n_periods > self.rate_hz/4:
@@ -113,10 +208,11 @@ class SteeringSenderJoystick:
                     velocity = velocity/4.0
                 if abs(velocity) >= 0.5:
                     velocity = np.sign(velocity) * (0.5/4 + (abs(velocity) - 0.5)*2)
-                self._command.velocity = velocity
+                self._target_speed = velocity
                 angular_velocity = np.clip(-self.joystick.get_axis_state('z', 0), -1.0, 1.0)*np.pi/3.0
                 self._command.angular_velocity = angular_velocity
 
+        self._command.velocity += np.clip((self._target_speed - self._command.velocity), -self._max_acc, self._max_acc)
         get_event_bus('steering').send(make_event(_g_message_name, self._command))
 
 
