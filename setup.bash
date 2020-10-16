6af9e3e8504ed7765ebca1ddd9a92f3bc70780ba
Ian Sherman <ian.j.sherman@gmail.com>
2020-10-16 13:25:21 -0400

    Disable proto-lint on ARM64 (#134)

--- setup.bash
+++ setup.bash
@@ -11,6 +11,11 @@ export FARM_NG_ROOT=$( cd "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
 
 . $FARM_NG_ROOT/env/bin/activate
 
+# Some pre-commit checks not supported on ARM
+if [ `dpkg --print-architecture` == "arm64" ]; then
+  export SKIP=proto-lint
+fi
+
 export BLOBSTORE_ROOT=`dirname $FARM_NG_ROOT`/tractor-data
 export PYTHONPATH=$FARM_NG_ROOT/python:$FARM_NG_ROOT/python/genproto:$FARM_NG_ROOT/env/lib
 export LD_LIBRARY_PATH=$FARM_NG_ROOT/env/lib
